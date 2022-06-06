6a043a1d04214b80592b03bade3a0b83feed17b4
Elton Chou <plscd748@gmail.com>
2022-06-06 19:32:50 +0000

    remove unused code

--- app/tests/test_model/test_source_checksum.py
+++ app/tests/test_model/test_source_checksum.py
@@ -8,21 +8,6 @@ from sqlalchemy.orm import Session
 
 @pytest.mark.usefixtures("db")
 class TestSourceChecksum:
-    def test_fetch_checksum_by_source(self, db: Session):
-        checksum = "kappa"
-        s = SourceChecksum(
-            source=SourceSite.GSC_ANNOUNCEMENT,
-            checksum=checksum
-        )
-        db.add(s)
-        db.commit()
-
-        site_checksum = SourceChecksum.get_by_site(
-            db, SourceSite.GSC_ANNOUNCEMENT)
-        assert site_checksum
-        assert site_checksum.checksum == checksum
-        assert isinstance(site_checksum.checked_at, datetime)
-
     def test_sourcechecksum_as_unique(self, db: Session):
         checksum = "kappa"
         s = SourceChecksum(
