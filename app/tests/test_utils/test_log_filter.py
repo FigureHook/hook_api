4f824b752a05294c04f9430c2b63cb39396fc366
Elton Chou <plscd748@gmail.com>
2022-07-26 16:55:29 +0000

    add log filter util

---
+++ app/tests/test_utils/test_log_filter.py
@@ -0,0 +1,50 @@
+from logging import LogRecord, INFO
+import pytest
+from app.utils.log_filter import (
+    AccessApplicationFilter,
+    application_name,
+    application_uuid
+)
+from faker import Faker
+
+
+@pytest.fixture
+def uuid(faker: Faker):
+    """Set and return uuid of application"""
+    uuid = faker.uuid4()
+    application_uuid.set(uuid)
+    return uuid
+
+
+@pytest.fixture
+def app_name(faker: Faker):
+    """Set and return name of application"""
+    name = faker.name()
+    application_name.set(name)
+    return name
+
+
+@pytest.fixture
+def log_record():
+    """Create a log record at INFO-level."""
+    record = LogRecord(
+        name="",
+        level=INFO,
+        pathname="",
+        lineno=0,
+        msg="Kappa",
+        args=(),
+        exc_info=None
+    )
+    return record
+
+
+def test_filter_adds_application_info(app_name, uuid, log_record):
+    filter_ = AccessApplicationFilter()
+
+    assert not hasattr(log_record, 'application_name')
+    assert not hasattr(log_record, 'application_uuid')
+
+    filter_.filter(log_record)
+    assert log_record.application_name == app_name
+    assert log_record.application_uuid == uuid
---
+++ app/utils/log_filter.py
@@ -0,0 +1,28 @@
+from contextvars import ContextVar
+from logging import Filter
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from logging import LogRecord
+
+
+application_uuid: ContextVar[Optional[str]] = ContextVar(
+    'application_uuid',
+    default=None
+)
+application_name: ContextVar[Optional[str]] = ContextVar(
+    'application_name',
+    default=None
+)
+
+
+class AccessApplicationFilter(Filter):
+    """Logging filter to attached application name and UUIDs to log records"""
+
+    def filter(self, record: 'LogRecord') -> bool:
+        """
+        Attach the accessing application name and UUID to the log record.
+        """
+        record.application_uuid = application_uuid.get()  # type: ignore[attr-defined]
+        record.application_name = application_name.get()  # type: ignore[attr-defined]
+        return True
